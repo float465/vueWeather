6678d0d1500c248130be8b0096da089370552da2
admin <golozopikgomik@gmail.com>
2025-02-06 17:02:01 +0300

    инициализация

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,44 @@
 <script>
+import axios from 'axios'
+
 export default {
 	data() {
 		return {
-			city: ""
+			city: "",
+			error: "",
+			info: null
+		}
+	},
+	computed: {
+		cityName() {
+			return this.city
+		},
+		showTemp() {
+			return "Температура: " + this.info.main.temp
+		},
+		showFeelsLike() {
+			return "Ощущается как: " + this.info.main.feels_like
+		},
+		showMinTemp() {
+			return "Минимальная температура: " + this.info.main.temp_min
+		},
+		showMaxTemp() {
+			return "Максимальная температура: " + this.info.main.temp_max
+		},
+
+	},
+	methods: {
+		getWeather() {
+			if(this.city.trim().length < 2) {
+				this.error = "Нужно название более одного символа :)"
+				return false
+			}
+
+			this.error = ""
+
+			axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${this.city}&units=metric&appid=64007cb8a701ec79f75917dcb4d3c76e`)
+				.then(res => (this.info = res.data))
+
 		}
 	}
 }
@@ -11,13 +47,29 @@ export default {
 <template>
 	<div class="wrapper">
 		<h1>Погодное приложение</h1>
-		<p>Узнать погоду в {{ city == "" ? "вашем городе" : city }}</p>
+		<p>Узнать погоду в {{ city == "" ? "вашем городе" : cityName }}</p>
 		<input type="text" v-model="city" placeholder="Введите город">
-		<button>Получить погоду</button>
+		<button v-show="city != ''" @click = "getWeather()">Получить погоду</button>
+		<p class="error">{{ error }}</p>
+
+		<div class="a" v-if="info != null">
+			<p>{{ info.main.temp }}</p>
+			<p>{{ showTemp }}</p>
+			<p>{{ showFeelsLike }}</p>
+			<p>{{ showMinTemp }}</p>
+			<p>{{ showMaxTemp }}</p>
+		</div>
 	</div>
 </template>
 
 <style scoped>
+
+
+
+.error{
+	color: rgb(223, 141, 145);
+	font-size: 13px;
+}
 	.wrapper{
 		width: 900px;
 		height: 500px;
